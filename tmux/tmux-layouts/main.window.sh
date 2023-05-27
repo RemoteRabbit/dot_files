7d71ebbf1cef1ec6789ec2f53f062c1b684fa0b2
Tristan Jahnke <tristan.jahnke@wendys.com>
2023-05-27 14:27:29 -0500

    tmux layout updates

--- tmux/tmux-layouts/main.window.sh
+++ tmux/tmux-layouts/main.window.sh
@@ -1,6 +1,5 @@
 # Set window root path. Default is `$session_root`.
 # Must be called before `new_window`.
-#window_root "~/wendys/DevOps/SRE/"
 
 # Create new window. If no argument is given, window name will be based on
 # layout file name.
