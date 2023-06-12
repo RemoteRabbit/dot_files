5a432314d469289dd17ddc2cabd342b4a31b119e
Tristan Jahnke <trjahnke@gmail.com>
2023-06-11 20:09:45 -0500

    updated homebrew install script

--- scripts/homebrew.sh
+++ scripts/homebrew.sh
@@ -1,14 +1,38 @@
 #!/bin/bash
 
+echo "Checking for homebrew..."
 if test ! "$(which brew)"; then
-    echo "Installing homebrew..."
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	echo "Installing homebrew..."
+	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
+echo "Homebrew found!"
 
 echo "Installing homebrew packages..."
 
-brew install starship
+declare -a packages=(
+	"awscli"
+	"cmatrix"
+	"docker"
+	"git"
+	"helm"
+	"kubernetes-cli"
+	"minikube"
+	"neofetch"
+	"neovim"
+	"node"
+	"pipx"
+	"python"
+	"python3"
+	"qemu"
+	"starship"
+	"terraform"
+	"tflint"
+	"tmux"
+	"virt-manager"
+	"virtualenv"
+)
 
-
-
-# Setup OS specific ones if needed
+for package in "${packages[@]}"; do
+	echo "Installing $package..."
+	brew install "$package"
+done
