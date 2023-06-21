0be87e4c8ca7c30ee5a059da1c47b559e5b48358
Tristan Jahnke <trjahnke@gmail.com>
2023-06-21 16:16:14 -0500

    add steam rule

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -530,18 +530,18 @@ awful.rules.rules = {
 	-- Add titlebars to normal clients and dialogs
 	{ rule_any = { type = { "normal", "dialog" } }, properties = { titlebars_enabled = true } },
 
-	-- {
-	-- 	rule_any = {
-	-- 		class = { "Steam" },
-	-- 	},
-	-- 	properties = {
-	-- 		titlebars_enabled = false,
-	-- 		floating = true,
-	-- 		border_width = 0,
-	-- 		border_color = 0,
-	-- 		size_hints_honor = false,
-	-- 	},
-	-- },
+	{
+		rule_any = {
+			class = { "Steam" },
+		},
+		properties = {
+			titlebars_enabled = false,
+			floating = true,
+			border_width = 0,
+			border_color = 0,
+			size_hints_honor = false,
+		},
+	},
 	-- Set Firefox to always map on the tag named "2" on screen 1.
 	-- { rule = { class = "Firefox" },
 	--   properties = { screen = 1, tag = "2" } },
