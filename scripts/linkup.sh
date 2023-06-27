16eb11f88d709295b48e0fd0cb6e6634f0c4e10c
Tristan Jahnke <trjahnke@gmail.com>
2023-06-26 20:21:58 -0500

    add in sysmlinker

---
+++ scripts/linkup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+dirs() {
+	for f in $(find "$HOME"/dot_files/.config/* -maxdepth 0); do
+		if [[ $f != "zsh" ]]; then
+			link "$f"
+		else
+			files "$f"
+		fi
+	done
+}
+
+files() {
+	for f in $(find "$1" -maxdepth 1 -type f); do
+		link "$f"
+	done
+}
+
+link() {
+	ln -sfv "$f" "$HOME"/.config/
+}
+
+dirs
