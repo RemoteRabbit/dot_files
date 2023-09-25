ae6428c899fda717929683ec796f6a51060b48b6
Tristan Jahnke <trjahnke@gmail.com>
2023-09-24 19:28:27 -0500

    base tmux setup

--- README.md
+++ README.md
@@ -1,36 +1,37 @@
 [![wakatime](https://wakatime.com/badge/github/RemoteRabbit/dot_files.svg)](https://wakatime.com/badge/github/RemoteRabbit/dot_files)
 
 # dot_files
+
 Central storage of dot files for easy pulling and use on other machines.
 
 ## Resources
+
 - Great culmination of other resources for [tmux](https://github.com/rothgar/awesome-tmux#plugins)
 - [Packer](https://github.com/wbthomason/packer.nvim) for neovim
 - Setting up [gpg](https://gist.github.com/ankurk91/c4f0e23d76ef868b139f3c28bde057fc) for git signing
 - Like the Tmux resource but for [neovim](https://github.com/rockerBOO/awesome-neovim)
 - Text to [ansi art](https://patorjk.com/software/taag/#p=display&f=ANSI%20Shadow&t=dotfiles)
 
-## Arch Depends
-- bluez
-- bluez-utils
-- rofi
+## Tmux
+
+TPM (Tmux Package Manager)
+`git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm`
 
 # Software
 
-| Category | Name | Docs/Site |
-| -------- | ---- | --------- |
-| Terminal | alcritty |  |
-| Font | 3720 Nerd Font |  |
-| Colors | catppuccin |  |
-| Shell | zsh |  |
-| Multiplexer | zellij |  |
-| Editor | Neovim |  |
-| Git | base git |  |
+| Category    | Name           | Docs/Site |
+| ----------- | -------------- | --------- |
+| Terminal    | alcritty       |           |
+| Font        | 3720 Nerd Font |           |
+| Colors      | catppuccin     |           |
+| Shell       | zsh            |           |
+| Multiplexer | zellij         |           |
+| Editor      | Neovim         |           |
+| Git         | base git       |           |
 
 # Hardware
 
-| Category | What | Link |
-| -------- | ---- | ---- |
-| Keyboard | Kenisis Advatage Pro 360 |  |
-| Mouse | Logitech Mx Master 3 | |
-
+| Category | What                     | Link |
+| -------- | ------------------------ | ---- |
+| Keyboard | Kenisis Advatage Pro 360 |      |
+| Mouse    | Logitech Mx Master 3     |      |
--- nvim/lua/Lazy/plugins/nvim-tree.lua
+++ nvim/lua/Lazy/plugins/nvim-tree.lua
@@ -1,38 +1,38 @@
 return {
-	"nvim-tree/nvim-tree.lua",
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-	},
-	config = function()
-		local nvimtree = require("nvim-tree")
-		-- disable netrw at the very start of your init.lua
-		vim.g.loaded_netrw = 1
-		vim.g.loaded_netrwPlugin = 1
+  "nvim-tree/nvim-tree.lua",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    local nvimtree = require("nvim-tree")
+    -- disable netrw at the very start of your init.lua
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
 
-		-- set termguicolors to enable highlight groups
-		vim.opt.termguicolors = true
+    -- set termguicolors to enable highlight groups
+    vim.opt.termguicolors = true
 
-		nvimtree.setup({
-			sort_by = "case_sensitive",
-			view = {
-				width = 30,
-			},
-			renderer = {
-				group_empty = true,
-			},
-			filters = {
-				dotfiles = true,
-			},
-		})
-		local wk = require("which-key")
-		wk.register({
-			e = {
-				name = "nvim-tree",
-				e = { "<cmd>NvimTreeToggle<CR>", "Toggle file explorer" },
-				f = { "<cmd>NvimTreeFindFileToggle<CR>", "Toggle file explorer on current file" },
-				c = { "<cmd>NvimTreeCollapse<CR>", "Collapse file explorer" },
-				r = { "<cmd>NvimTreeRefresh<CR>", "Refresh file explorer" },
-			},
-		}, { prefix = "<leader>" })
-	end,
+    nvimtree.setup({
+      sort_by = "case_sensitive",
+      view = {
+        width = 30,
+      },
+      renderer = {
+        group_empty = true,
+      },
+      filters = {
+        dotfiles = false,
+      },
+    })
+    local wk = require("which-key")
+    wk.register({
+      e = {
+        name = "nvim-tree",
+        e = { "<cmd>NvimTreeToggle<CR>", "Toggle file explorer" },
+        f = { "<cmd>NvimTreeFindFileToggle<CR>", "Toggle file explorer on current file" },
+        c = { "<cmd>NvimTreeCollapse<CR>", "Collapse file explorer" },
+        r = { "<cmd>NvimTreeRefresh<CR>", "Refresh file explorer" },
+      },
+    }, { prefix = "<leader>" })
+  end,
 }
