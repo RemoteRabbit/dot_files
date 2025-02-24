48ab280a43b34818cc80157ce5fd8b6b69e9799b
Tristan Jahnke <trjahnke@protonmail.com>
2025-02-23 18:55:20 -0600

    refactor: cleanup

--- NeoVim/lua/plugins/zenmode.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "folke/zen-mode.nvim",
-  opts = {
-  },
-  config = function()
-    -- local zen = require("zen-mode.nvim")
-    vim.keymap.set("n", "<leader>zm", require("zen-mode").toggle, { desc = "Toggle Zen Mode" })
-  end,
-}
